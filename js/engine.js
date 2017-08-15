0a24ccf4630d5fc486bbbab2e2127b6f3c427f69
kidManuel <cascallarmanuel@gmail.com>
2017-08-15 01:02:25 -0300

    Major rework to namespacing logic and function exposition.

--- js/engine.js
+++ js/engine.js
@@ -1,26 +1,33 @@
 window.loader = (function() {
-    return {
-        setPath: function(path) {
-            this.path = path;
-        },
-        getPath: function() {
-            return this.path;
-        },
-        load: function(filename, path) {
-            var output;
-            if (!filename) {
-                throw new Error('no filename provided')
-            }
-            var path = this.path || path || 'resources'
-            $.ajax('./' + path + '/' + filename + '.svg', {
-                success: function(data) {
-                    out = data.getElementsByTagName('svg')[0];
-                    document.getElementsByTagName('body')[0].appendChild(out)
-                },
-                error: function() {
-                    console.log('you fucked up')
-                }
-            })
+    function setPath(path) {
+        this.path = path;
+    }
+
+    function getPath() {
+        return this.path;
+    }
+
+    function load(filename, callback, path) {
+        var output;
+        if (!filename) {
+            throw new Error('no filename provided')
         }
+        var path = this.path || path || 'resources'
+        $.ajax('./' + path + '/' + filename + '.svg', {
+            success: function(data) {
+                out = data.getElementsByTagName('svg')[0];
+                callback(out);
+            },
+            error: function() {
+                console.log('you fucked up')
+            }
+        })
+    }
+
+        }
+    return {
+        getPath: getPath,
+        setPath: setPath,
+        load: load,
     }
 })();
