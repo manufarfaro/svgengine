a67ead66a921448d65dcd171b698cb1c1aedd7d1
kidManuel <cascallarmanuel@gmail.com>
2017-08-16 22:39:00 -0300

    linted engine.

--- js/engine.js
+++ js/engine.js
@@ -1,14 +1,14 @@
 window.loader = (function() {
-    window.obj = document.getElementById('testObj')
-    var setup = {
+    window.obj = document.getElementById('testObj');
+    let setup = {
         path: null,
         useCache: false,
-        defaultClass: null
-    }
+        defaultClass: null,
+    };
 
     Element.prototype.svgInsert = function(svg, config) {
-        placeInObject(this, svg, config)
-    }
+        placeInObject(this, svg, config);
+    };
 
     function setPath(path) {
         setup.path = path;
@@ -19,44 +19,47 @@ window.loader = (function() {
     }
 
     function load(filename, callback, callbackExtraArguments, path) {
-        var output;
         if (!filename) {
-            throw new Error('no filename provided')
+            throw new Error('no filename provided');
         }
-        var path = this.path || path || 'resources'
+        var path = this.path || path || 'resources';
         $.ajax('./' + path + '/' + filename + '.svg', {
             success: function(data) {
                 out = data.getElementsByTagName('svg')[0];
                 callback(out, callbackExtraArguments);
             },
             error: function() {
-                console.log('you fucked up')
-            }
-        })
+                console.log('you fucked up');
+            },
+        });
     }
 
     function placeInObject(object, svgName, config) {
         if (!object instanceof Node) {
-            throw new Error('not a valid html element')
+            throw new Error('not a valid html element');
         }
         load(svgName, function(mySvg, config) {
             object.appendChild(prepSVG(mySvg, config));
-        }, config)
+        }, config);
     }
 
     function prepSVG(svg, config) {
-        var extraClass =  config.class || setup.defaultClass;
+        let extraClass = config.class || setup.defaultClass;
         if (extraClass) {
             svg.classList.add(extraClass);
         }
         return svg;
     }
+
+
+    function addCSSProperty(key, value) {
+
     }
 
     return {
         getPath: getPath,
         setPath: setPath,
         load: load,
-        placeInObject: placeInObject
-    }
+        placeInObject: placeInObject,
+    };
 })();
